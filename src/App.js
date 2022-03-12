2be0ca2decf8cb491b6283a1d77bedf44a6a602e
Mr-Zee <911mrzee@gmail.com>
2022-03-13 00:29:42 +0530

    Navbar Started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNavbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar/>
+      {/* Main */}
+      {/* Footer */}
     </div>
   );
 }
