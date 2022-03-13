9429281e0ab8505884f20d1a8c57b6937e6d97cb
Mr-Zee <911mrzee@gmail.com>
2022-03-13 19:48:05 +0530

    aligned set

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -13,60 +13,97 @@ function MainComponent() {
     return (
         <Container fluid="md" className="mainComponent">
             <Row>
-                <Col>
+                <Col className="bannerLeft d-none d-md-block">
                     <img className="heroImg" src={hero} alt="" />
                 </Col>
                 <Col className="heroTitleBox">
-                    <h2 className="heroTitle">DONATE BLOOD <p>&</p> SAVE LIFE</h2>
+                    <h2 className="heroTitle">DONATE BLOOD <br />& <br /> SAVE LIFE</h2>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <p className="newsContent"><b> NEWS : </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quis totam nihil neque velit in!</p>
+                    <marquee behavior="" direction="" className="newsContent">
+                        <b> NEWS : </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quis totam nihil neque velit in!</marquee>
                 </Col>
             </Row>
             <Row>
                 <Col className="bloodStock">
                     <p>Blood Stocks</p>
-                    <div className='circle'>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                        <Col className='dot'>A+</Col>
-                    </div>
+                    <Row className='circle'>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> A+ </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> A- </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> AB+ </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> AB- </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> B+ </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> B- </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> O+ </p>
+                            </div>
+                        </Col>
+                        <Col xs={6} sm={6} md={4} lg={3} className='d-flex justify-content-center my-4'>
+                            <div className='dot d-flex justify-content-center align-items-center'>
+                                <p> O- </p>
+                            </div>
+                        </Col>
+
+
+                    </Row>
                 </Col>
             </Row>
-            <Row>
-                <div className='statistics'>
-                    <Col className='statEle'>
-                        {/* <img className="" src={logo} alt="" />  */}
-                        <Icon className='icon' icon={campsiteIcon} />
+            <Row className='statistics py-5 '>
+                <Col>
+                    <div className='statEle m-auto'>
+                        <Icon className='statIcon mb-3' icon={campsiteIcon} />
                         <p>50+</p>
                         <p>Camps</p>
-                    </Col>
-                    <Col className='statEle'>
-                        {/* <img className="" src={logo} alt="" />  */}
-                        <Icon className='icon' icon={peopleAdd16Regular} />
+                    </div>
+                </Col>
+                <Col >
+                    <div className='statEle m-auto'>
+                        <Icon className='statIcon mb-3' icon={peopleAdd16Regular} />
                         <p>1000+</p>
-                        <p>Regestered</p>
-                    </Col>
-                    <Col className='statEle'>
-                        {/* <img className="" src={logo} alt="" />  */}
-                        <Icon className='icon' icon={donateBlood} />
+                        <p>Registered</p>
+                    </div>
+                </Col>
+                <Col >
+                    <div className='statEle m-auto'>
+                        <Icon className='statIcon mb-3' icon={donateBlood} />
                         <p>500+</p>
                         <p>Donors</p>
-                    </Col>
-                    <Col className='statEle'>
-                        {/* <img className="" src={logo} alt="" />  */}
-                        <Icon className='icon' icon={handsHelping} />
+                    </div>
+                </Col>
+                <Col >
+                    <div className='statEle m-auto'>
+                        <Icon className='statIcon mb-3' icon={handsHelping} />
                         <p>20+</p>
                         <p>Volunteers</p>
-                    </Col>
-                </div>
+                    </div>
+                </Col>
             </Row>
         </Container>
     );
