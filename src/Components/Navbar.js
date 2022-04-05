9698054181867f5c6ca3a268d26a3729eb5d8fc7
Mr-Zee <911mrzee@gmail.com>
2022-04-05 22:45:45 +0530

    dynamic Change LogIn

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,29 +3,38 @@ import logo from "../Assets/img/logo.png";
 import { Link } from "react-router-dom";
 
 function CustomNavbar() {
+    let isLoggedIn = false;
     return (
         <Navbar className="CustomNavbar" bg="light" expand="lg">
             <Container>
                 <Link to="/">
                     <img className='mainLogo' src={logo} alt="" />
-                    <Navbar.Brand href="#home"> <span  className='px-2'>Blood Bank</span></Navbar.Brand>
+                    <Navbar.Brand href="#home"> <span className='px-2'>Blood Bank</span></Navbar.Brand>
                     <Navbar.Text>
                         <Link to="/"><span className='px-2' >Home</span> </Link>
                     </Navbar.Text>
-                    <Navbar.Text>
-                        <Link to="/dashboard"><span className='px-2' >Dashboard</span> </Link>
-                    </Navbar.Text>
-                    <Navbar.Text>
-                        <Link to="/registration"><span className='px-2' >Registration</span></Link>
-                    </Navbar.Text>
-                    <Navbar.Text>
-                        <Link to="/profiles"><span className='px-2' >Profiles</span></Link>
-                    </Navbar.Text>
+                    {
+                        isLoggedIn ? <><Navbar.Text>
+                            <Link to="/dashboard"><span className='px-2' >Dashboard</span> </Link>
+                        </Navbar.Text>
+                            <Navbar.Text>
+                                <Link to="/registration"><span className='px-2' >Registration</span></Link>
+                            </Navbar.Text>
+                            <Navbar.Text>
+                                <Link to="/profiles"><span className='px-2' >Profiles</span></Link>
+                            </Navbar.Text></> : null
+                    }
                 </Link>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
-                        <Link to="/login">Login</Link>
+                        {
+                            isLoggedIn ? <>
+                                <Link to="/login">Logout</Link>
+                            </> : <>
+                                <Link to="/login">Login</Link>
+                            </>
+                        }
                     </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
